b8ed49a5ba43f3834f3bdba3de7c631ee9a8cd57
Neilson Gamble <neilgamb@Neilsons-MacBook-Pro.local>
2017-04-14 12:34:26 -0400

    Send button added.  Send/Receive formatting added.

--- chat.js
+++ chat.js
@@ -7,27 +7,19 @@ function getMessages() {
 
         let response = JSON.parse(request.responseText);
 
-            for(let i = 0; i < response.chats.length; i++){
+            for(let i = totalMessages; i < response.chats.length; i++){
                     if (response.chats.length === totalMessages){
                     } else {
                         addMessage(response.chats[i]);
                     }
             }     
-
     });
 
     request.send();
 }
 
-function sendMessage() {
-    let request = new XMLHttpRequest();
-    request.open('POST', 'https://tiy-28202.herokuapp.com/chats');
-
-    request.send()
-}
-
 function addMessage(message) {
-        
+
     let parent = document.querySelector('.convo');
     parent.classList.add('convo');
 
@@ -48,7 +40,7 @@ function addMessage(message) {
 
     let sender = document.createElement('p');
     sender.classList.add('sender');
-    sender.textContent = message.from;
+    sender.textContent = "You";
 
     let timestamp = document.createElement('p');
     timestamp.classList.add('timestamp');
@@ -75,15 +67,36 @@ function addMessage(message) {
 
     let timestampRec = document.createElement('p');
     timestampRec.classList.add("timestamp");
+
+    let unix = Date.parse(message.added);
+    let date = new Date(unix);
     timestampRec.textContent = message.added;
 
-    parent.appendChild(received);
-    totalMessages = totalMessages + 1;
-    received.appendChild(receivedmeta);
-    receivedmeta.appendChild(senderRec);
-    receivedmeta.appendChild(timestampRec);
-    received.appendChild(bubbleRec);
-    bubbleRec.appendChild(textRec);
+    let name = document.querySelector('#name');
+
+    console.log(name);
+    console.log(name.value === message.from)
+
+    if (name.value === message.from){
+            parent.appendChild(sent);
+            totalMessages = totalMessages + 1;
+            sent.appendChild(bubble);
+            bubble.appendChild(text);  
+            sent.appendChild(sentmeta);
+            sentmeta.appendChild(senderRec);
+            sentmeta.appendChild(timestamp);
+          
+
+    } else {
+            parent.appendChild(received);
+            totalMessages = totalMessages + 1;
+            received.appendChild(receivedmeta);
+            receivedmeta.appendChild(senderRec);
+            receivedmeta.appendChild(timestampRec);
+            received.appendChild(bubbleRec);
+            bubbleRec.appendChild(textRec);
+    }
+
 
 }
 
@@ -96,6 +109,18 @@ sendBtn.addEventListener('click', function() {
     let message = document.querySelector('#message');
     console.log(name.value);
     console.log(message.value);
+
+    let request = new XMLHttpRequest();
+    request.open('POST', 'https://tiy-28202.herokuapp.com/chats');
+
+    request.addEventListener('load', function() {
+        console.log('received response');
+
+    })
+    request.send(JSON.stringify({
+        from: name.value,
+        message: message.value,
+    }));
 
 })
 
